e7373a0c48e8715dff84e42283836393d6720cb9
Igor Dias <igorverissimo02@hotmail.com>
2023-08-17 20:58:48 -0300

    Fix #46: movendo a tela ao deixar o botão pressionado

--- js/controller.js
+++ js/controller.js
@@ -4,3 +4,13 @@ function mover(long, lat){
     let novo_centro = L.latLng(centro.lat + lat, centro.lng + long);
     map.setView(novo_centro);
 }
+let movimentacao
+function iniciar_movimentacao(long, lat){
+    movimentacao = setInterval(function() {
+                mover(long, lat);
+            }, 100);
+}
+
+function finalizar_movimentacao(long, lat){
+    clearInterval(movimentacao)
+}
