341827ac42351037a80fa43edb8ffa3594fa817b
matheusslr <matheusslr@hotmail.com>
2023-08-18 09:59:17 -0300

    fix: index carrossel

--- js/controller.js
+++ js/controller.js
@@ -1,16 +1,16 @@
-function mover(long, lat){
-
+function mover(long, lat) {
     let centro = map.getCenter();
     let novo_centro = L.latLng(centro.lat + lat, centro.lng + long);
     map.setView(novo_centro);
 }
+
 let movimentacao
-function iniciar_movimentacao(long, lat){
-    movimentacao = setInterval(function() {
-                mover(long, lat);
-            }, 100);
+function iniciar_movimentacao(long, lat) {
+    movimentacao = setInterval(function () {
+        mover(long, lat);
+    }, 100);
 }
 
-function finalizar_movimentacao(long, lat){
+function finalizar_movimentacao(long, lat) {
     clearInterval(movimentacao)
 }
